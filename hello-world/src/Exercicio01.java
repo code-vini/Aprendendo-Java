99e1c576963bfde7138d2d96237710d14e7154d0
Vinicius Silva <viniciusbatista.16.07@gmail.com>
2024-10-11 19:56:02 -0400

    Mais um dia apredizado e resolvendo exercicios de Java!

--- hello-world/src/Exercicio01.java
+++ hello-world/src/Exercicio01.java
@@ -7,6 +7,9 @@ public class Exercicio01 {
             saudacao();
             soma();
             repetindofor();
+            repetindowhile();
+            manips();
+            //fazendometodu();
         }
     
             // Definição do método - Exercicio 01 -  Olá Mundo
@@ -31,6 +34,34 @@ public class Exercicio01 {
                         System.out.println("Repetição de: "+ gg);
                   }
             }
+            /*/* ### **Exercício 4: Estrutura de Repetição (while)**
+            1. **Escreva um programa que imprime os números de 1 a 10 usando um loop** 
+            `while`**.***/ 
+            public static void repetindowhile(){
+                  for (int vin = 1; vin <= 10; vin++){
+                        System.out.println(vin);
+                  } 
+            }
+            /* ### **Exercício 5: Introdução a Métodos**
+            1. **Escreva um programa que possui um método para calcular 
+            o quadrado de um número e imprime o resultado.** 
+            public static void fazendometodu(){
+                  int numero = 5;
+                  int resultado = quadrado(numero);
+                  System.out.println("O quadrado de " + numero + " é: " + resultado);
+                  Não deu certo ;(
+            */
+            /*### **Exercício 6: Manipulação de Strings**
 
-
-}
+            1. **Escreva um programa que conta o número de caracteres em uma string fornecida pelo usuário.**
+            
+             */
+            public static void manips(){
+                  Scanner scannerr = new Scanner(System.in);
+                  System.out.println("Digite uma palavra ou frase: ");
+                  String texto = scannerr.nextLine();
+                  int comprimento = texto.length();
+                  System.out.println("Número de carecteres: " + comprimento);
+            }
+            
+      }
